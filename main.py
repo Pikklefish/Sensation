1fbfcd9af853cbdf1c799aefcdc7df57b4c14180
Pikklefish <nsaspetersong@gmail.com>
2024-09-06 18:52:30 +0900

    grid search

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.svm import SVC
 from sklearn.metrics import classification_report
 from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV
 
 ## Import Dataset ##
 file_path = 'C:/Users/work/Sensation/reduced_tweet_dataset.csv'
@@ -71,17 +72,25 @@ X_train_vect = vectorizer.fit_transform(X_train)
 x_test_vect = vectorizer.transform(x_test)
 print(f"Vectorization finished in {time.time() - start_time} seconds")
 
+
+## Set Parameter ##
+param_grid = {'C': [0.01, 0.1, 1, 10]}
+
 ## SVM model ##
-svm_model = SVC(kernel = 'linear', cache_size=2000, verbose = True)
+svm_model = SVC(kernel = 'linear', cache_size=1000, verbose = True)
+grid_search = GridSearchCV(svm_model, param_grid, cv=5, verbose=2)
 print("training started")
 start_time = time.time()
-svm_model = svm_model.fit(X_train_vect, Y_train)
+grid_search.fit(X_train_vect, Y_train)
 print(f"Training finished in {time.time() - start_time} seconds")
-y_pred = svm_model.predict(x_test_vect)
+print(f"Best C value: {grid_search.best_params_['C']}")
+
+best_svm_model = grid_search.best_estimator_
+y_pred = best_svm_model.predict(x_test_vect)
 
 print(classification_report(y_test, y_pred))
 
 # Save the model
 model_file_path = "svm_model.pkl"
-joblib.dump(svm_model, model_file_path)
+joblib.dump(best_svm_model, model_file_path)
 print(f"Model saved to {model_file_path}")
